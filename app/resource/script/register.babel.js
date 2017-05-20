9e5aa5b0dbe67eb14119df93cb372544c0ecd086
江肖 <407505475@qq.com>
2017-05-20 11:44:45 +0800

    注册页

---
+++ app/resource/script/register.babel.js
@@ -0,0 +1,75 @@
+(function($){
+  $('#register').on('click',function() {
+
+    const username = $("input[name='username']").val()
+    const userpwd = $("input[name='userpwd']").val()
+    const confirm_userpwd = $("input[name='confirm-userpwd']").val()
+    const code = $("input[name='code']").val()
+    const school = $("input[name='school']").val()
+    const room = $("input[name='room']").val()
+    const detailaddr = $("input[name='detailaddr']").val()
+
+    if( username === '') {
+      alert('请输入手机号')
+      return
+    } 
+    else if(/^1[0-9]{10}$/.test(username) === false) {
+      alert('手机号格式不正确')
+			return
+    }
+    else if (userpwd === '') {
+      alert('请输入密码')
+      return
+    }
+    else if (confirm_userpwd === '') {
+      alert('请确认密码')
+      return
+    }
+    else if (confirm_userpwd != userpwd) {
+      alert('两次密码不一致')
+      return
+    }
+    else if (code === '') {
+      alert('请输入验证码')
+      return
+    }
+    else if (school === '') {
+      alert('请输入学校')
+      return
+    }
+    else if (room === '') {
+      alert('请输入宿舍')
+      return
+    }
+    else if (detailaddr === '') {
+      alert('请输入详细地址')
+      return
+    }
+    else if (detailaddr.length < 5) {
+      alert('详细地址长度少于5个字')
+      return
+    }
+
+    $.ajax({
+      url: '',
+      type: "POST",
+
+      data: {
+        username: username,
+        userpwd: userpwd,
+        confirm_userpwd: confirm_userpwd,
+        code: code,
+        school: school,
+        room: room,
+        detailaddr: detailaddr,
+      },
+
+      success:function(data) {
+
+        if (data.status === 1) {
+          window.location.href = '/home/login';
+        }
+      }
+    })
+  })
+})(jQuery);
